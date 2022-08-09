57814040fa8aa5bedf245da6c57b2cb7b5cb6e56
Wesley Reynolds <42392208+WesReynolds@users.noreply.github.com>
2022-08-09 15:59:32 -0700

    Create SortKeysByASCValue.java

---
+++ src/main/java/csc369/SortKeysByASCValue.java
@@ -0,0 +1,41 @@
+package csc369;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class SortKeysByASCValue {
+
+    public static final Class OUTPUT_KEY_CLASS = IntWritable.class;
+    public static final Class OUTPUT_VALUE_CLASS = Text.class;
+
+    public static class MapperImpl extends Mapper<LongWritable, Text, IntWritable, Text> {
+	private final IntWritable one = new IntWritable(1);
+	private Text word = new Text();
+
+        @Override
+	protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+		StringTokenizer itr = new StringTokenizer(value.toString());
+		Text newValue = new Text(itr.nextToken().toString());
+		IntWritable newKey = new IntWritable(Integer.parseInt(itr.nextToken().toString()));
+		context.write(newKey, newValue);
+        }
+    }
+
+    public static class ReducerImpl extends Reducer<IntWritable, Text, Text, IntWritable> {
+	private IntWritable result = new IntWritable();
+    
+        @Override
+	protected void reduce(IntWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {        
+            for (Text value : values) {
+              context.write(value, key);
+            }
+       }
+    }
+}

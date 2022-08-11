f650d180df7e7912ffdb6afcf67b67f781e4d6fa
Wesley Reynolds <42392208+WesReynolds@users.noreply.github.com>
2022-08-11 11:14:13 -0700

    Update SortTuples.java

--- src/main/java/csc369/SortTuples.java
+++ src/main/java/csc369/SortTuples.java
@@ -12,33 +12,44 @@ import org.apache.hadoop.mapreduce.Reducer;
 
 public class SortTuples {
 
-	public static final Class OUTPUT_KEY_CLASS = Text.class;
-	public static final Class OUTPUT_VALUE_CLASS = IntWritable.class;
+	public static final Class OUTPUT_KEY_CLASS = CountryCountPair.class;
+	public static final Class OUTPUT_VALUE_CLASS = Text.class;
 
-	public static class MapperImpl extends Mapper<LongWritable, Text, Text, IntWritable> {
-		private final IntWritable one = new IntWritable(1);
-		private Text word = new Text();
-
+	public static class MapperImpl extends Mapper<LongWritable, Text, CountryCountPair, Text> {
 		@Override
 		protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-			context.write(value, one);			
+			String record[] = value.toString().split(" ");
+			
+			String country = record[0];
+			String url = record[1];
+			int count = Integer.parseInt(record[2]);
+						
+			context.write(new CountryCountPair(country, count), new Text(url));			
+		}
+	}
+	
+	// used to perform secondary sort on temperature
+	public static class SortComparator extends WritableComparator {
+		protected SortComparator() {
+			super(CountryCountPair.class, true);
+		}
+
+		@Override
+		public int compare(WritableComparable wc1, WritableComparable wc2) {
+			CountryCountPair pair = (CountryCountPair) wc1;
+			CountryCountPair pair2 = (CountryCountPair) wc2;
+			return pair.compareTo(pair2);
 		}
 	}
 
-	public static class ReducerImpl extends Reducer<Text, IntWritable, Text, IntWritable> {
-		private IntWritable result = new IntWritable();
-
+	public static class ReducerImpl extends Reducer<CountryCountPair, Text, Text, IntWritable> {
 		@Override
-		protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-			int sum = 0;
+		protected void reduce(CountryCountPair key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+			String country = key.getCountry();
+			int count = key.getCount();
 			
-			for (IntWritable value : values) {
-				sum += value.get();
-			}
-
-			if (sum > 0) {
-				result.set(sum);
-				context.write(key, result);
+			for (Text value : values) {
+				context.write(new Text(country + " " + value), new IntWritable(count));
 			}
 		}
 	}
